3ff88ff599b9f68d9e6828ccfd434c54eca66d1e
alibotTEST <43972327+alibotTEST@users.noreply.github.com>
2018-10-19 15:53:26 +0300

    Update alitestbot.js

--- alitestbot.js
+++ alitestbot.js
@@ -73,52 +73,51 @@ client.on('message', function(message) {
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setUsername(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "stream")) {
 		        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setGame(args , 'https://www.twitch.tv/ali_ar95');
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "playing")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setGame(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "listen")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setActivity(args, {type:'LISTENING'});
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "watch")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setActivity(args, {type:'WATCHING'});
         message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "setavatar")) {
 				        if(message.author.id !== myID) return;
         client.user.setAvatar(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
                 if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-           msg.delete(3000);
-          message.delete(3000);
+           msg.delete(5000);
+          message.delete(5000);
         });
     }
 });
-
 
 
 client.on('message', message => {
